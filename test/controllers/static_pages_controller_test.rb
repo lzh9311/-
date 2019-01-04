4261282d88cb1010483d22d11f9f13df62a3524a
sqing <763693955@qq.com>
2019-01-04 06:34:02 +0000

    更新css

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,24 +2,24 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = "SampleApp"
+    @base_title = "数据集"
   end
   
   
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select 'title',"#{@base_title}"
+    assert_select 'title',"主页|#{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select 'title',"#{@base_title}"
+    assert_select 'title',"帮助|#{@base_title}"
   end
 test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title',"#{@base_title}"
+    assert_select 'title',"关于|#{@base_title}"
   end
 end
