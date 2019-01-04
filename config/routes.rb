d2a36eff5e9a2eb480a930797c6279f5ba35d2a1
sqing <763693955@qq.com>
2019-01-04 12:24:51 +0000

    建立一个基本用户模型

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
     root "static_pages#home"
     #具名路由，更方便调用与修改
     get "/help", to:"static_pages#help"
     get "/about", to:"static_pages#about"
     get "/contact", to:"static_pages#contact"
+    get "/signup", to:"users#new"
 end
